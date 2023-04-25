a7e544554dd9ffbce19ca505013efaab5f6d9ec6
Arul Ajmani <arulajmani@gmail.com>
2023-04-25 17:08:49 -0400

    WIP, DNM: debug

--- pkg/upgrade/upgrades/key_visualizer_migration.go
+++ pkg/upgrade/upgrades/key_visualizer_migration.go
@@ -14,16 +14,8 @@ import (
 	"context"
 
 	"github.com/cockroachdb/cockroach/pkg/clusterversion"
-	"github.com/cockroachdb/cockroach/pkg/jobs"
-	"github.com/cockroachdb/cockroach/pkg/jobs/jobspb"
-	"github.com/cockroachdb/cockroach/pkg/keys"
 	// Import keyvisjob so that its init function will be called.
 	_ "github.com/cockroachdb/cockroach/pkg/keyvisualizer/keyvisjob"
-	"github.com/cockroachdb/cockroach/pkg/security/username"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog/systemschema"
-	"github.com/cockroachdb/cockroach/pkg/sql/isql"
-	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/upgrade"
 )
 
@@ -32,60 +24,5 @@ import (
 func keyVisualizerTablesMigration(
 	ctx context.Context, _ clusterversion.ClusterVersion, d upgrade.SystemDeps,
 ) error {
-
-	tables := []catalog.TableDescriptor{
-		systemschema.SpanStatsUniqueKeysTable,
-		systemschema.SpanStatsBucketsTable,
-		systemschema.SpanStatsSamplesTable,
-		systemschema.SpanStatsTenantBoundariesTable,
-	}
-
-	for _, table := range tables {
-		err := createSystemTable(ctx, d.DB.KV(), d.Settings, keys.SystemSQLCodec,
-			table)
-		if err != nil {
-			return err
-		}
-	}
-
-	// These migrations are skipped for backup tests because these tables are not part
-	// of the backup.
-	shouldBootstrapJob := true
-	shouldConfigureTTL := true
-
-	if knobs := d.KeyVisKnobs; knobs != nil {
-		shouldBootstrapJob = !knobs.SkipJobBootstrap
-		shouldConfigureTTL = !knobs.SkipZoneConfigBootstrap
-	}
-
-	// Set the TTL for the SpanStatsTenantBoundariesTable table.
-	if shouldConfigureTTL {
-		if _, err := d.DB.Executor().ExecEx(
-			ctx,
-			"set-SpanStatsTenantBoundariesTable-TTL",
-			nil,
-			sessiondata.InternalExecutorOverride{User: username.RootUserName()},
-			"ALTER TABLE system.span_stats_tenant_boundaries CONFIGURE ZONE USING gc.ttlseconds = $1",
-			3600, /* one hour */
-		); err != nil {
-			return err
-		}
-	}
-
-	if shouldBootstrapJob {
-		record := jobs.Record{
-			JobID:         jobs.KeyVisualizerJobID,
-			Description:   "key visualizer job",
-			Username:      username.NodeUserName(),
-			Details:       jobspb.KeyVisualizerDetails{},
-			Progress:      jobspb.KeyVisualizerProgress{},
-			NonCancelable: true, // The job can't be canceled, but it can be paused.
-		}
-
-		return d.DB.Txn(ctx, func(ctx context.Context, txn isql.Txn) error {
-			return d.JobRegistry.CreateIfNotExistAdoptableJobWithTxn(ctx, record, txn)
-		})
-	}
-
 	return nil
 }
